c3be11aa3feb8e807a32646ad6d97b9e7306296c
Jorge Sz <saenzdiazj0@gmail.com>
2023-09-19 23:45:55 -0500

    Add get, add, findByID User

--- src/main/java/org/tech/mycvtech/service/StudentService.java
+++ src/main/java/org/tech/mycvtech/service/StudentService.java
@@ -2,7 +2,11 @@ package org.tech.mycvtech.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.tech.mycvtech.model.User;
 import org.tech.mycvtech.repository.UserMongoRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -11,5 +15,17 @@ public class StudentService {
     @Autowired
     public StudentService(UserMongoRepository userMongoRepository) {
         this.mongoRepository = userMongoRepository;
+    }
+
+    public User addUser(User newUser) {
+        return mongoRepository.save(newUser);
+    }
+
+    public List<User> allUsers() {
+        return mongoRepository.findAll();
+    }
+
+    public Optional<User> findById(Long userId) {
+        return mongoRepository.findById(userId);
     }
 }
