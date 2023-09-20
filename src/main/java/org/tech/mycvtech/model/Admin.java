64c389588e9c33d3bf59a82506a225c4fc0c9b1c
Jorge Sz <saenzdiazj0@gmail.com>
2023-09-19 23:15:34 -0500

    Add User:Admin Class

--- src/main/java/org/tech/mycvtech/model/Admin.java
+++ src/main/java/org/tech/mycvtech/model/Admin.java
@@ -1,4 +1,39 @@
 package org.tech.mycvtech.model;
 
-public class Admin extends User{
+public class Admin extends User {
+    public String accessLevel;
+    public String department;
+
+    public Admin(
+            Long id,
+            String username,
+            String firstName,
+            String lastName,
+            String email,
+            String phone,
+            String password,
+            String rol,
+            String accessLevel,
+            String department
+    ) {
+        super(id, username, firstName, lastName, email, phone, password, rol);
+        this.accessLevel = accessLevel;
+        this.department = department;
+    }
+
+    public String getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(String accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
