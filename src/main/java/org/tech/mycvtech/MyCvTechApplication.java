af89541b62ac2ae030f02c126712c4033ee74970
Jorge Sz <saenzdiazj0@gmail.com>
2023-09-19 20:31:56 -0500

    First Commit

---
+++ src/main/java/org/tech/mycvtech/MyCvTechApplication.java
@@ -0,0 +1,13 @@
+package org.tech.mycvtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCvTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCvTechApplication.class, args);
+    }
+
+}
