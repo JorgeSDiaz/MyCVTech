3b6d3ff0574ff6a021a9efa376cd9044da4d48d7
Jorge Sz <saenzdiazj0@gmail.com>
2023-09-22 21:45:37 -0500

    Cors

--- src/main/java/org/tech/mycvtech/config/SecurityConfiguration.java
+++ src/main/java/org/tech/mycvtech/config/SecurityConfiguration.java
@@ -21,6 +21,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
